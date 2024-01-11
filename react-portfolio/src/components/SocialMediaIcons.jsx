53dff02557483bb865bc2696ebe32c15a9065485
Dylan Matthews <djmatthews93@gmail.com>
2024-01-11 15:43:43 -0700

    socials added to landing

---
+++ react-portfolio/src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src="/twitter.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src="/instagram.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
--- react-portfolio/src/scenes/Landing.jsx
+++ react-portfolio/src/scenes/Landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import SocialMediaIcons from "../components/SocialMediaIcons";
 
 function Landing({ setSelectedPage }) {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -85,6 +86,20 @@ function Landing({ setSelectedPage }) {
               Let's talk.
             </div>
           </AnchorLink>
+        </motion.div>
+
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
         </motion.div>
       </div>
     </section>
